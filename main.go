b587b37dd7be5dded8c36ce635eb9547aa4b61a9
David Bariod <davidriod@googlemail.com>
2022-09-09 17:29:12 +0200

    add the :week period parameter for the list subcommand

--- main.go
+++ main.go
@@ -97,6 +97,15 @@ func (cmd *ListCmd) Run(cfg *CommonConfig) error {
 		year, month, day := startTime.Date()
 		startTime = time.Date(year, month, day, 0, 0, 0, 0, time.Local)
 		stopTime = time.Date(year, month, day+1, 0, 0, 0, 0, time.Local)
+	case ":week":
+		year, month, day := startTime.Date()
+		weekday := startTime.Weekday()
+		if weekday == time.Sunday {
+			weekday = time.Saturday + 1
+		}
+		startTime = time.Date(year, month, day-int(weekday-time.Monday), 0, 0, 0, 0, time.Local)
+		stopTime = time.Date(year, month, day+1+int(time.Saturday+1-weekday), 0, 0, 0, 0, time.Local)
+		fmt.Println(startTime, stopTime)
 	default:
 		return fmt.Errorf("this period is not yet implemented: %s", cmd.Period)
 	}

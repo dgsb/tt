a1b8c3055a9074079d32ffa333bd9f993e3a661c
David Bariod <davidriod@googlemail.com>
2022-11-16 09:30:36 +0100

    add total time sum in the list command output

--- report.go
+++ report.go
@@ -27,6 +27,7 @@ func FlatReport(tas []db.TaggedInterval, out io.Writer) error {
 	tab := tabwriter.NewWriter(out, 16, 4, 0, ' ', 0)
 
 	var prevStartTime time.Time
+	var totalDuration time.Duration
 	for _, ta := range tas {
 		if !sameDate(prevStartTime, ta.Interval.StartTimestamp) {
 			tab.Write([]byte(ta.Interval.StartTimestamp.Format("2006-01-02")))
@@ -42,7 +43,9 @@ func FlatReport(tas []db.TaggedInterval, out io.Writer) error {
 		if ta.Interval.StopTimestamp.IsZero() {
 			ta.Interval.StopTimestamp = time.Now().Truncate(time.Second)
 		}
-		tab.Write([]byte(ta.Interval.StopTimestamp.Sub(ta.Interval.StartTimestamp).String()))
+		duration := ta.Interval.StopTimestamp.Sub(ta.Interval.StartTimestamp)
+		totalDuration += duration
+		tab.Write([]byte(duration.String()))
 		tab.Write([]byte("\t"))
 
 		tab.Write([]byte(strings.Join(ta.Tags, ",")))
@@ -52,6 +55,11 @@ func FlatReport(tas []db.TaggedInterval, out io.Writer) error {
 
 		prevStartTime = ta.Interval.StartTimestamp
 	}
+	tab.Write([]byte("\n"))
+	tab.Write([]byte("Total time"))
+	tab.Write([]byte("\t\t\t\t"))
+	tab.Write([]byte(totalDuration.String()))
+	tab.Write([]byte("\n"))
 	tab.Flush()
 
 	return nil
